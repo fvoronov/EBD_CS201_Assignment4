9afe52d1cde2e8adcb9d1d956dc96d5042ed90a5
fvoronov <f.voronov@kse.org.ua>
2026-07-21 15:56:58 +0300

    added calculated colon net_profit to list_of_dictionaries

--- main.py
+++ main.py
@@ -11,10 +11,18 @@ with open("regional_tariffs.json", "r", encoding="utf-8") as f:
 for element in list_of_dictionaries:
     if element["quantity"] == "N/A":
         element["quantity"] = 0
+    else:
+        element["quantity"] = int(element["quantity"])
     if element["revenue"] == "N/A":
         element["revenue"] = 0
+    else:
+        element["revenue"] = float(element["revenue"])
 
 for region, tariff in dictionary.items():
     if tariff == "N/A":
         dictionary[region] = 0
 
+for sales in list_of_dictionaries:
+    for region, tariff in dictionary.items():
+        if sales["region"] == region:
+            sales["net_profit"] = sales["revenue"] - (sales["revenue"] * (tariff / 100))
