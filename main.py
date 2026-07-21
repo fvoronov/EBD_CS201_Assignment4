2e986c9095b78520e4ae962adb5c92e716e8b60e
fvoronov <f.voronov@kse.org.ua>
2026-07-21 15:10:58 +0300

    data cleaned, all "N/A"'s changed to 0

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ with open("global_sales.csv", "r", encoding="utf-8") as f:
 with open("regional_tariffs.json", "r", encoding="utf-8") as f:
     dictionary = json.load(f)
 
+for element in list_of_dictionaries:
+    if element["quantity"] == "N/A":
+        element["quantity"] = 0
+    if element["revenue"] == "N/A":
+        element["revenue"] = 0
+
+for region, tariff in dictionary.items():
+    if tariff == "N/A":
+        dictionary[region] = 0
+
