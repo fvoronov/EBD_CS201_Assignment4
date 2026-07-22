f998fba6492b12fa0f2a6d1d19e64b03c8de7c0d
fvoronov <f.voronov@kse.org.ua>
2026-07-22 19:32:34 +0300

    created cleaned_sales_updated.csv with net_prices column

--- main.py
+++ main.py
@@ -26,3 +26,11 @@ for sales in list_of_dictionaries:
     for region, tariff in dictionary.items():
         if sales["region"] == region:
             sales["net_profit"] = sales["revenue"] - (sales["revenue"] * (tariff / 100))
+
+fieldnames = list(list_of_dictionaries[0].keys())
+
+with open("cleaned_sales_updated.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(list_of_dictionaries)
+
